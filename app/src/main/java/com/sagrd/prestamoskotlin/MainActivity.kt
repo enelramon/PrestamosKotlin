fda30e6c0f300f1ada842a82fb247697eb8c2166
Enel Almonte <enelramon@hotmail.com>
2022-04-12 13:13:15 -0400

    Proyecto inicial con dependencias Room y Hilt

---
+++ app/src/main/java/com/sagrd/prestamoskotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sagrd.prestamoskotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
